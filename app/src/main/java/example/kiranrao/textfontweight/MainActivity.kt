2aaa2aa73a67a6a286510b7c97c3220bd4bfadc7
Kiran Rao <curioustechizen@users.noreply.github.com>
2020-06-26 13:59:44 +0200

    First commit

---
+++ app/src/main/java/example/kiranrao/textfontweight/MainActivity.kt
@@ -0,0 +1,12 @@
+package example.kiranrao.textfontweight
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_main)
+    }
+}
